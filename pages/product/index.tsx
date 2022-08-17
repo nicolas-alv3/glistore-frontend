1c894b57b90943e630703f3674fc1b424706c95b
NA433420 <nicolas.alvarez@redb.ee>
2022-08-17 17:39:00 -0300

    Add validation and feedback in add to cart

--- pages/product/index.tsx
+++ pages/product/index.tsx
@@ -1,4 +1,4 @@
-import { useRouter } from 'next/router'
+import {useRouter} from 'next/router'
 import React, {useEffect} from "react";
 import ProductService from "../../service/ProductService";
 import {
@@ -24,36 +24,37 @@ import Skeleton from "react-loading-skeleton";
 function ShowProductSkeleton() {
     return <>
         <Grid.Column width={6}>
-            <Skeleton style={{width: "min(400px, 100%)", height:300}}/>
+            <Skeleton style={{width: "min(400px, 100%)", height: 300}}/>
         </Grid.Column>
         <Grid.Column width={6}>
             <Skeleton height={24} style={{marginBottom: "16px"}}/>
-            <Skeleton height={16} count={4} />
+            <Skeleton height={16} count={4}/>
             <Skeleton height={24} width={64} style={{margin: "16px 0"}}/>
-            <Skeleton height={16}  width={128}/>
-            <Skeleton height={48}  width={152}  style={{margin: "48px 0"}}/>
+            <Skeleton height={16} width={128}/>
+            <Skeleton height={48} width={152} style={{margin: "48px 0"}}/>
         </Grid.Column>
     </>
 }
 
 const ProductDetail = () => {
     const router = useRouter()
-    const { id } = router.query
+    const {id} = router.query
     const [loading, setLoading] = React.useState<boolean>(true);
     const [product, setProduct] = React.useState<Product>();
     const [amount, setAmount] = React.useState(1);
     const [talle, setTalle] = React.useState("");
+    const [formSubmitted, setFormSubmitted] = React.useState(false);
     const dispatch = useDispatch()
 
 
-    useEffect( () => {
-        ProductService.getProductById(String(id)).then( p => {
+    useEffect(() => {
+        ProductService.getProductById(String(id)).then(p => {
             setProduct(p);
             setLoading(false);
         })
     }, [id]);
 
-    const addToCart = () => {
+    const addCorrect = () => {
         const cartItem = {
             product,
             amount,
@@ -64,50 +65,63 @@ const ProductDetail = () => {
         dispatch(show());
     }
 
-    // @ts-ignore
-    // @ts-ignore
-    return <>
-    <Container>
-        <Title title={"Ver producto"} withBackButton />
-        <Grid stackable>
-            <Grid.Row>
-                {
-                    loading ? <ShowProductSkeleton /> :
-                        <>
-                            <Grid.Column width={6}>
-                                <Carrousel urls={product?.images || []} />
-                            </Grid.Column>
-                            <Grid.Column width={6}>
-                                <Header size={"huge"}>{product?.name} <Label color={"orange"}>{product?.category}</Label></Header>
-                                <CardDescription>{product?.description}</CardDescription>
-                                <Header className={styles.font} size={"huge"}>${product?.price}</Header>
-                                <Header className={styles.font} size={"medium"}>Disponible en talles:</Header>
-                                {/* eslint-disable-next-line react/jsx-key */}
-                                {product?.talles?.map(t => <Button
-                                    key={t}
-                                    color={"orange"}
-                                    basic={talle !== t}
-                                    onClick={ () => setTalle(t)}
-                                >{t}
-                                </Button>)}
-                                <Divider/>
-                                <Header className={styles.font} size={"small"}>Cantidad:</Header>
-                                <Button icon={"plus"} onClick={() => setAmount(prevAmount => prevAmount + 1)}/>
-                                <Input type={"number"} value={amount} error={amount <= 0} />
-                                <Button icon={"minus"} onClick={() => setAmount(prevAmount => prevAmount - 1)}/>
-                                <Divider/>
-                                <Button color={"brown"} size={"large"} disabled={!Boolean(talle) || amount < 1} onClick={addToCart}>
-                                    <Icon name={"cart plus"} />
-                                    Agregar al carrito
-                                </Button>
-                            </Grid.Column>
-                        </>
-                }
+    const addToCart = () => {
+        if (amount > 0 && talle) {
+            addCorrect()
+        } else {
+            setFormSubmitted(true);
+            ToastUtils.error("Debes elegir un talle y cantidad primero!")
+        }
+    }
 
-            </Grid.Row>
-        </Grid>
-        <TrendingSwiper />
-    </Container>
+    const isInvalidAmount = amount <= 0 && formSubmitted
+
+    const isInvalidTalle = !talle && formSubmitted
+
+    return <>
+        <Container>
+            <Title title={"Ver producto"} withBackButton/>
+            <Grid stackable>
+                <Grid.Row>
+                    {
+                        loading ? <ShowProductSkeleton/> :
+                            <>
+                                <Grid.Column width={6}>
+                                    <Carrousel urls={product?.images || []}/>
+                                </Grid.Column>
+                                <Grid.Column width={6}>
+                                    <Header size={"huge"}>{product?.name} <Label
+                                        color={"orange"}>{product?.category}</Label></Header>
+                                    <CardDescription>{product?.description}</CardDescription>
+                                    <Header className={styles.font} size={"huge"}>${product?.price}</Header>
+                                    <Header className={styles.font} size={"medium"}>Disponible en talles:</Header>
+                                    {isInvalidTalle && <div style={{color:"red"}}>Debes elegir un talle</div>}
+                                    {product?.talles?.map(t => <Button
+                                        key={t}
+                                        color={"orange"}
+                                        basic={talle !== t}
+                                        onClick={() => setTalle(t)}
+                                    >{t}
+                                    </Button>)}
+                                    <Divider/>
+                                    <Header className={styles.font} size={"small"}>Cantidad:</Header>
+                                    {isInvalidAmount && <div style={{color:"red"}}>La cantidad debe ser mayor a cero</div>}
+                                    <Button icon={"plus"} onClick={() => setAmount(prevAmount => prevAmount + 1)}/>
+                                    <Input type={"number"} value={amount} error={amount <= 0}/>
+                                    <Button icon={"minus"} onClick={() => setAmount(prevAmount => prevAmount - 1)}/>
+                                    <Divider/>
+                                    <Button color={"brown"} size={"large"} onClick={addToCart}>
+                                        <Icon name={"cart plus"}/>
+                                        Agregar al carrito
+                                    </Button>
+                                </Grid.Column>
+                            </>
+                    }
+
+                </Grid.Row>
+            </Grid>
+            <TrendingSwiper/>
+        </Container>
     </>
 }
 
