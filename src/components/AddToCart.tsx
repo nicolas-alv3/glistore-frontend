8d84a64e5a59f1fe15c3ecefabbb44f34f829f56
Nico Alvarez (NA433420) <nicolas.alvarez@redb.ee>
2022-09-17 17:06:09 -0300

    Update call to sales and whastapp message sending

--- src/components/AddToCart.tsx
+++ src/components/AddToCart.tsx
@@ -30,8 +30,9 @@ export default function AddToCart(props: Props) {
             product: props.product,
             amount,
             talle,
-            features: Object.keys(features).map( k => ({ name: k, value: features[k]}))
+            features: Object.keys(features).map( k => ({ name: k, value: features[k].toString()}))
         }
+        console.log(cartItem)
         dispatch(addItem(cartItem as unknown as SaleItem));
         props.onAdd && props.onAdd();
         ToastUtils.success("Perfecto!")
