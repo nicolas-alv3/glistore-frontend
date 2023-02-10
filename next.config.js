7ef0cb4217037df507d65a6df0bf75096b7bf9fd
Nico Alvarez (NA433420) <nicolas.alvarez@redb.ee>
2023-02-10 13:52:59 -0300

    Update path to api v1

--- next.config.js
+++ next.config.js
@@ -9,7 +9,7 @@ const nextConfig = {
     return [
       {
         // matching all API routes
-        source: "/api/:path*",
+        source: "/api/v1/:path*",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
           { key: "Access-Control-Allow-Origin", value: "*" },
--- service/SearchService.ts
+++ service/SearchService.ts
@@ -2,7 +2,7 @@ import API from "./api";
 import {SearchRequest} from "../src/types";
 
 class SearchService {
-    path = "/products"
+    path = "/api/v1/products"
     search({name, filter, page, pageSize}: SearchRequest) {
         const params = `?category=${filter.categories.toString()}&name=${name}&page=${page}&pageSize=${pageSize}]`
         return API.get(`${this.path}/${params}`);
