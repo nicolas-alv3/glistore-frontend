66cccbf28d9e95506eb24e1b0e9364c06d4f3ada
Nico Alvarez <nicolas.alv3@gmail.com>
2023-02-11 14:47:39 -0300

    change next config.json

--- service/SearchService.ts
+++ service/SearchService.ts
@@ -4,7 +4,7 @@ import {SearchRequest} from "../src/types";
 class SearchService {
     path = "/api/v1/products"
     search({name, filter, page, pageSize}: SearchRequest) {
-        const params = `?category=${filter.categories.toString()}&name=${name}&page=${page}&pageSize=${pageSize}]`
+        const params = `?category=${filter.categories.toString()}&name=${name}&page=${page}&pageSize=${pageSize}`
         return API.get(`${this.path}/${params}`);
     }
 }
